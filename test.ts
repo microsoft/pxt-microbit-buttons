c609f58ef5ca3509fa1af9aa6bc2696b1d2adaea
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-12-07 06:52:35 -0800

    Update pxt.json, README.md, touch.ts, test.ts

--- test.ts
+++ test.ts
@@ -1,13 +1,13 @@
 // tests go here; this will not be compiled when this package is used as an extension.
 basic.forever(function () {
-    if (input.touchP0.isTouched()) {
+    if (touch.pinP0.isTouched()) {
         led.plot(0, 0)
     } else {
         led.unplot(0, 0)
     }
 })
 basic.forever(function () {
-    if (input.touchP2.isTouched()) {
+    if (touch.pinP2.isTouched()) {
         led.plot(4, 0)
     } else {
         led.unplot(4, 0)
--- touch.ts
+++ touch.ts
@@ -2,7 +2,7 @@
 /**
  * User interaction on buttons
  */
-const enum ButtonEvent {
+const enum TouchButtonEvent {
     //% block="click"
     Click = 3 /* DAL.MICROBIT_BUTTON_EVT_CLICK */,
     //% block="long click"
@@ -13,32 +13,29 @@ const enum ButtonEvent {
     Down = 1 /* DAL.MICROBIT_BUTTON_EVT_DOWN */
 };
 
-namespace input {
+/**
+ * Capacitive button support in micro:bit
+ */
+namespace touch {
     const CAPACITIVE_TOUCH_ID = 1200;
     const CAP_SAMPLES_2 = 3;
 
-    class ButtonBase {
-        private id: number;
-        constructor(id: number) {
-            this.id = id;
-        }
-    }
-
+    /**
+     * A self capacitive button mounted on micro:bit pins.
+     */
     //% fixedInstances
     export class CapacitiveButton {
         private id: number;
         private pin: AnalogInOutPin;
-        private calibration = -1;
-        private threshold = 3;
+        private calibration: number;
+        private threshold: number;
+        private lastReading: number;
 
         constructor(id: number, pin: AnalogInOutPin) {
             this.id = id;
             this.pin = pin;
-        }
-
-        value() {
-            this.init();
-            return this.read();
+            this.lastReading = -1;
+            this.calibration = -1;
         }
 
         private read() {
@@ -49,36 +46,77 @@ namespace input {
                 this.pin.digitalWrite(true);
                 basic.pause(1);
             }
-            reading = reading >> CAP_SAMPLES_2;
-            return reading;
-        }
-
-        calibrate() {
-            this.calibration = this.read();
-            this.threshold = 3;
-        }
-
-        isTouched(): boolean {
-            this.init();
-            const reading = this.value();
-            const state = this.calibration + this.threshold < reading;
-            return state;
-        }
-
-        onEvent(event: ButtonEvent, handler: () => void) {
-            control.onEvent(this.id, event, handler);
+            this.lastReading = reading >> CAP_SAMPLES_2;
+            return this.lastReading;
         }
 
         private init() {
-            if (this.calibration < 0)
-                this.calibrate();
+            const startWorker = this.lastReading < 0;
+            // calibrate if needed
+            if (this.calibration < 0) {
+                const reading = this.read();
+                this.calibration = reading;
+                this.threshold = 3;
+            }
+            if (startWorker) {
+                // never initialized
+                // TODO: onIdle
+                control.inBackground(() => this.idleWorker());
+            }
+        }
+
+        private idleWorker() {
+            while (true) {
+                // don't interfere with calibration
+                if (this.calibration >= 0)
+                    this.read();
+                basic.pause(20)
+            }
+        }
+
+        /**
+         * Gets the value of the capacitive pin
+         */
+        //% blockId=touchvalue block="%button value"
+        value() {
+            this.init();
+            return this.lastReading;
+        }
+
+        /**
+         * Calibrate
+         */
+        //% blockId=touchcalibrate block="%button calibrate"
+        calibrate() {
+            this.calibration = -1;
+            this.init();
+        }
+
+        /**
+         * Determines if the button is being pressed
+         */
+        //% blockId=touchistouched block="is %button touched"
+        isTouched(): boolean {
+            this.init();
+            const state = this.calibration + this.threshold
+                < this.lastReading;
+            return state;
+        }
+
+        /**
+         * Registers an button event
+         */
+        //% blockId=touchonevent block="on %button $event"
+        onEvent(event: TouchButtonEvent, handler: () => void) {
+            this.init();
+            control.onEvent(this.id, event, handler);
         }
     }
 
-    //% fixedInstance block="touch P0"
-    export const touchP0 = new CapacitiveButton(CAPACITIVE_TOUCH_ID + DAL.MICROBIT_ID_IO_P0, pins.P0);
-    //% fixedInstance block="touch P1"
-    export const touchP1 = new CapacitiveButton(CAPACITIVE_TOUCH_ID + DAL.MICROBIT_ID_IO_P1, pins.P1);
-    //% fixedInstance block="touch P2"
-    export const touchP2 = new CapacitiveButton(CAPACITIVE_TOUCH_ID + DAL.MICROBIT_ID_IO_P2, pins.P2);
+    //% fixedInstance block="P0"
+    export const pinP0 = new CapacitiveButton(CAPACITIVE_TOUCH_ID + DAL.MICROBIT_ID_IO_P0, pins.P0);
+    //% fixedInstance block="P1"
+    export const pinP1 = new CapacitiveButton(CAPACITIVE_TOUCH_ID + DAL.MICROBIT_ID_IO_P1, pins.P1);
+    //% fixedInstance block="P2"
+    export const pinP2 = new CapacitiveButton(CAPACITIVE_TOUCH_ID + DAL.MICROBIT_ID_IO_P2, pins.P2);
 }
