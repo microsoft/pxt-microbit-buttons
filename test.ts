b13db852072d52da56dc71863fecf2d3f87aa8d8
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-02-10 17:22:06 -0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,5 +1,6 @@
 // tests go here; this will not be compiled when this package is used as an extension.
 led.plot(2, 0)
+/*
 basic.forever(function () {
     console.logValue("p0t", input.touchP0.threshold)
     console.logValue("p0", input.touchP0.value())
@@ -8,27 +9,30 @@ basic.forever(function () {
     } else {
         led.unplot(0, 0)
     }
-    if (input.touchP2.isTouched()) {
-        led.plot(4, 0)
-    } else {
-        led.unplot(4, 0)
-    }
 })
-input.touchP0.onEvent(TouchButtonEvent.Down, function () {
-    for (let i = 0; i < 5; ++i)
-        led.unplot(i, 1)
-    led.plot(0, 1)
-})
-input.touchP0.onEvent(TouchButtonEvent.Up, function () {
-    led.unplot(0,1)
-    led.plot(1, 1)
-})
-input.touchP0.onEvent(TouchButtonEvent.Click, function () {
-    led.plot(2, 1)
-})
-input.touchP0.onEvent(TouchButtonEvent.LongClick, function () {
-    led.plot(3, 1)
-})
-input.touchP0.onEvent(TouchButtonEvent.Hold, function () {
-    led.plot(4, 1)
-})
+*/
+function bind(pin: input.CapacitiveButton, row: number) {
+    pin.onEvent(TouchButtonEvent.Down, function () {
+        for (let i = 0; i < 5; ++i)
+            led.unplot(i, row)
+        led.plot(0, row)
+    })
+    pin.onEvent(TouchButtonEvent.Up, function () {
+        led.unplot(0, row)
+        led.plot(1, row)
+    })
+    pin.onEvent(TouchButtonEvent.Click, function () {
+        led.plot(2, row)
+    })
+    pin.onEvent(TouchButtonEvent.LongClick, function () {
+        led.plot(3, row)
+    })
+    pin.onEvent(TouchButtonEvent.Hold, function () {
+        led.plot(4, row)
+    })
+}
+
+bind(input.touchP1, 1)
+
+
+
